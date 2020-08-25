1ce0c6a3ebc3a70607d56e36150791a298882b2e
Zachary Churchill <zacharyachurchill@gmail.com>
2020-08-25 00:57:57 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use iced;
+use iced::{Container, Element, Length, Row, Sandbox, Settings, Svg};
+use std::env;
+mod types;
+use cauldron::audio::AudioSegment;
+use types::*;
+
+fn main() {
+    App::run(Settings::default())
+}
+
+struct App;
+
+impl Sandbox for App {
+    type Message = ();
+
+    fn new() -> Self {
+        App
+    }
+
+    fn title(&self) -> String {
+        String::from("Tundra")
+    }
+
+    fn update(&mut self, _message: ()) {}
+
+    fn view(&mut self) -> Element<()> {
+        let args: Vec<String> = env::args().collect();
+        let mut wave = WaveForm {
+            wave: AudioSegment::read(&args[1]).unwrap(),
+        };
+        let svg = wave.view();
+        let svg_container = Container::new(svg)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .padding(10)
+            .center_x()
+            .center_y();
+        let file_selector_container = Container::new(FileSelector::new().view())
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_x();
+        Row::new()
+            .push(svg_container)
+            .push(file_selector_container)
+            .into()
+    }
+}
---
+++ src/types.rs
@@ -0,0 +1,80 @@
+use cauldron::audio::AudioSegment;
+use iced::svg::Handle;
+use iced::{Column, Container, Element, Length, Row, Sandbox, Settings, Svg, Text};
+use std::fs::{self, DirEntry};
+use std::io;
+use std::ops::Div;
+use std::path::PathBuf;
+use svg::node::element::path::Data;
+use svg::Document;
+
+pub struct WaveForm {
+    pub wave: AudioSegment,
+}
+
+impl WaveForm {
+    pub fn view(&mut self) -> Svg {
+        let samples: Vec<i32> = self.wave.samples().unwrap().map(|r| r.unwrap()).collect();
+        let samples_len = samples.len();
+        let max = 2_i64.pow(self.wave.info().bits_per_sample);
+        let data = audio_to_svg(samples);
+        let path = svg::node::element::Path::new()
+            .set("fill", "none")
+            .set("stroke", "black")
+            .set("stroke-width", 5)
+            .set("d", data);
+        let document = Document::new()
+            .set("viewBox", (0, -max, samples_len, max * 2))
+            .set("preserveAspectRatio", "none")
+            .add(path);
+        let svg_data: Vec<u8> = Vec::from(document.to_string());
+        Svg::new(Handle::from_memory(svg_data))
+    }
+}
+
+fn audio_to_svg(samples: Vec<i32>) -> Data {
+    // let truncate = 100; // (samples.len() as u64).div(100 as u64);
+    samples
+        .iter()
+        .enumerate()
+        // .filter(|&(i, _)| (i as u64).div(truncate) == 0)
+        .fold(Data::new(), |data, (i, s)| {
+            if i % 2 == 0 {
+                data.move_to((i, s.to_owned()))
+            } else {
+                data.line_to((i, s.to_owned()))
+            }
+        })
+        .close()
+}
+
+pub struct FileSelector {
+    pub selected_file: Option<PathBuf>,
+}
+
+impl FileSelector {
+    pub fn new() -> Self {
+        FileSelector {
+            selected_file: None,
+        }
+    }
+
+    pub fn view(self) -> Column<'static, ()> {
+        let dir = std::env::current_dir().unwrap();
+        fs::read_dir(dir).unwrap().fold(Column::new(), |col, path| {
+            let path = path.unwrap();
+            match path.path().to_str() {
+                Some(path_str) => col.push(
+                    Container::new(
+                        Text::new(path_str)
+                            .size(16)
+                            .width(Length::Fill)
+                            .height(Length::Fill),
+                    )
+                    .padding(5),
+                ),
+                None => col,
+            }
+        })
+    }
+}
