1ec00823eb6ba0ca934ef81bd139a36073580eb3
Zachary Churchill <zacharyachurchill@gmail.com>
2021-02-20 14:57:36 -0500

    remove DEBUG_BORDER_BOUDNS & BoxedContainer

--- src/types/style.rs
+++ src/types/style.rs
@@ -1,6 +1,4 @@
-use iced::{container, Background, Color, button};
-
-pub const DEBUG_BORDER_BOUNDS: BoxedContainer = BoxedContainer;
+use iced::{button, container, Background, Color};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Theme {
@@ -16,20 +14,6 @@ impl Default for Theme {
 
 ///////////////////////////////////////////////////////////////////////////
 
-pub struct BoxedContainer;
-
-impl container::StyleSheet for BoxedContainer {
-    fn style(&self) -> container::Style {
-        container::Style {
-            border_width: 3.0,
-            border_color: Color::from_rgb8(0xff as u8, 0x00 as u8, 0x00 as u8),
-            ..container::Style::default()
-        }
-    }
-}
-
-///////////////////////////////////////////////////////////////////////////
-
 pub struct SelectedContainer;
 
 impl container::StyleSheet for SelectedContainer {
@@ -37,9 +21,7 @@ impl container::StyleSheet for SelectedContainer {
         container::Style {
             text_color: Some(Color::from_rgb8(0xff as u8, 0xff as u8, 0xff as u8)),
             background: Some(Background::Color(Color::from_rgb8(
-                0x25 as u8,
-                0x7a as u8,
-                0xfd as u8,
+                0x25 as u8, 0x7a as u8, 0xfd as u8,
             ))),
             ..container::Style::default()
         }
@@ -61,9 +43,7 @@ impl button::StyleSheet for FileButton_ {
     fn hovered(&self) -> button::Style {
         button::Style {
             background: Some(Background::Color(Color::from_rgb8(
-                0xee as u8,
-                0xee as u8,
-                0xee as u8,
+                0xee as u8, 0xee as u8, 0xee as u8,
             ))),
             ..button::Style::default()
         }
