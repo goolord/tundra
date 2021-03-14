c0d4cfc547c6acf14646b3f7bb90ad5d41631353
Zachary Churchill <zacharyachurchill@gmail.com>
2021-03-14 01:03:35 -0500

    move app

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,9 +1,11 @@
-pub use common::*;
-pub use file_selector::*;
-pub use player::*;
-pub use style::*;
-
+mod app;
 mod common;
 mod file_selector;
 mod player;
 mod style;
+
+pub use app::*;
+pub use common::*;
+pub use file_selector::*;
+pub use player::*;
+pub use style::*;
